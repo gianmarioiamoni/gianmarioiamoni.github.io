f1cae6b2d1f2fa811437ba1427788e78b60a4b5f
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-07-06 16:57:26 +0200

    contact me form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 // Hamburger menu script for navigation
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
@@ -14,4 +15,38 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+// Send email script for contact form
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent reload
+
+    const serviceID = 'service_vaj153g';
+    const templateID = 'template_ra7qydm';
+    const publicKey = '8TX5EYp2A_heeHYUy';
+
+    emailjs.init(publicKey);
+
+    const templateParams = {
+        name: document.getElementById('name').value,
+        company: document.getElementById('company').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    };
+    
+    console.log("templateParams", templateParams);
+
+
+    emailjs.send(serviceID, templateID, templateParams)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Message sent successfully!');
+            document.getElementById('contact-form').reset();
+        }, function (error) {
+            console.log('FAILED...', error);
+            alert('Failed to send message. Please try again later.');
+        });
+});
+
 
